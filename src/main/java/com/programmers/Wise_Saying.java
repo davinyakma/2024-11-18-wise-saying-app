1ef571cb214624ee6ddebdb54865b84eb267a18c
davinyakma <poohcdv3652@naver.com>
2024-11-19 15:13:24 +0900

    Wise_Saying follow step1

--- src/main/java/com/programmers/Wise_Saying.java
+++ src/main/java/com/programmers/Wise_Saying.java
@@ -13,12 +13,20 @@ class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        //입력을 받음. 스캐너 설정 ()
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("명령) ");
-        String cmd = scanner.nextLine();
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
 
-        System.out.println("입력된 명령어 : %s".formatted(cmd));
+            //.formatted(cmd)는 +cmd 와 같은 표현임
+            //System.out.println("입력된 명령어 : %s".formatted(cmd));
+
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
 
         scanner.close();
     }
