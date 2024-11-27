676ae86079ff66a25ae7ed2c4483974e642cf6c6
davinyakma <poohcdv3652@naver.com>
2024-11-27 11:39:13 +0900

    Restart with Stream_lastWiseSaying(5 step)

--- src/main/java/com/programmers/Main.java
+++ src/main/java/com/programmers/Main.java
@@ -17,6 +17,8 @@ class App {
         Scanner scanner = new Scanner(System.in);
 
         int lastId = 0;
+        //초기값이 없어서 오류나니까 null로 초기화. Variable 'lastWiseSaying' might not have been initialized
+        WiseSaying lastWiseSaying = null; //WiseSaying 객체의 수명을 늘리기 위해 while문 밖에 생성
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -31,12 +33,19 @@ class App {
 
                 int id = ++lastId;
 
-                //명언에 같이 저장되는 데이터 패키징(묶음)
-                WiseSaying wiseSaying = new WiseSaying(); //빈 객체 생성. 목록을 저장하기 위한 객체
-                wiseSaying.id = id; //명언 번호
-                wiseSaying.content = content; //내용
-                wiseSaying.author = author; //작가
+                WiseSaying wiseSaying = new WiseSaying();
+                wiseSaying.id = id;
+                wiseSaying.content = content;
+                wiseSaying.author = author;
+
+                lastWiseSaying = wiseSaying; //while문까지만 사용 가능한 wiseSaying을 while문 바깥의 lastWiseSaying에 리모컨을 전달해줌. 수명 늘어남.
+
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+            } else if (cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                //최근 등록한 명언만 출력됨. wiseSaying가 하나여서 명언 한개만 저장 가능함.
+                System.out.println("%d / %s / %s".formatted(lastWiseSaying.id, lastWiseSaying.author, lastWiseSaying.content));
             }
         }
         scanner.close();
