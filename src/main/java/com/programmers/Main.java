9f0d03ac8a32ff0f7631d1cbde4e0cee86331f06
davinyakma <poohcdv3652@naver.com>
2024-11-27 09:37:01 +0900

    Restart with Stream_create App class(1 step)

--- src/main/java/com/programmers/Main.java
+++ src/main/java/com/programmers/Main.java
@@ -2,8 +2,18 @@ package com.programmers;
 
 public class Main {
     public static void main(String[] args) {
-        App app = new App();
-        app.run();
+        App app = new App(); //app이라는 레퍼런스 변수. App클래스의 객체 생성.
+        //그 객체의 주소값(리모컨)이 app에 저장!
+        app.run(); //ctrl + 1 하면 자동으로 필요한 run메소드 생성해줌.
+    }
+}
+
+class App {
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        System.out.print("명령) ");
     }
 }
 
