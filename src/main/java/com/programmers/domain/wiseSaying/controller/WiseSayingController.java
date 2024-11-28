e1cb99957142f0b300dc76e0005158211e0751ca
davinyakma <poohcdv3652@naver.com>
2024-11-28 20:20:44 +0900

    Restart with Stream_create WiseSayingController and separate WiseSayingController(11 step)

--- src/main/java/com/programmers/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/programmers/domain/wiseSaying/controller/WiseSayingController.java
@@ -1,34 +1,24 @@
 package com.programmers.domain.wiseSaying.controller;
 
 import com.programmers.domain.wiseSaying.entity.WiseSaying;
+import com.programmers.domain.wiseSaying.service.WiseSayingService;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class WiseSayingController {
+    private final WiseSayingService wiseSayingService;
     private final Scanner scanner;
-    private final List<WiseSaying> wiseSayings;
-    private int lastId;
 
     public WiseSayingController(Scanner scanner) {
-        this.wiseSayings = new ArrayList<>();
-        this.lastId = 0;
+        this.wiseSayingService = new WiseSayingService();
         this.scanner = scanner;
     }
 
-    public WiseSaying addWiseSaying(String content, String author) {
-        int id = ++lastId;
-
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-        wiseSayings.add(wiseSaying);
-
-        return wiseSaying;
-    }
-
     public void makeSampleData() {
-        addWiseSaying( "나의 죽음을 적들에게 알리지 말라", "이순신 장군");
-        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
+        wiseSayingService.addWiseSaying( "나의 죽음을 적들에게 알리지 말라", "이순신 장군");
+        wiseSayingService.addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
     }
 
     public  void actionAdd() {
@@ -36,7 +26,8 @@ public class WiseSayingController {
         String content = scanner.nextLine();
         System.out.print("작가 : ");
         String author = scanner.nextLine();
-        WiseSaying wiseSaying = addWiseSaying(content, author);
+
+        WiseSaying wiseSaying = wiseSayingService.addWiseSaying(content, author);
 
         System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
     }
@@ -44,6 +35,8 @@ public class WiseSayingController {
     public void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
+
+        List<WiseSaying> wiseSayings = wiseSayingService.findAll();
 
         for (WiseSaying wiseSaying : wiseSayings.reversed()) {
             System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
@@ -53,7 +46,7 @@ public class WiseSayingController {
         String idStr = cmd.substring(6);
         int id = Integer.parseInt(idStr);
 
-        boolean removed = wiseSayings.removeIf(wiseSaying -> wiseSaying.getId() == id);
+        boolean removed = wiseSayingService.removeById(id);
 
         if (removed) System.out.println("%d번 명언을 삭제했습니다.".formatted(id));
         else System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
@@ -63,19 +56,14 @@ public class WiseSayingController {
         String idStr = cmd.substring(6);
         int id = Integer.parseInt(idStr);
 
-        WiseSaying foundWiseSaying = null;
+        Optional<WiseSaying> opWiseSaying = wiseSayingService.findById(id);
 
-        for(WiseSaying wiseSaying : wiseSayings){
-            if(wiseSaying.getId() == id) {
-                foundWiseSaying = wiseSaying;
-                break;
-            }
-        }
-
-        if ( foundWiseSaying == null ) {
+        if ( opWiseSaying.isEmpty()) {
             System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
             return;
         }
+
+        WiseSaying foundWiseSaying = opWiseSaying.get();
 
         System.out.println("명언(기존) : %s" .formatted(foundWiseSaying.getContent()));
         System.out.print("명언 : ");
@@ -85,8 +73,7 @@ public class WiseSayingController {
         System.out.print("작가 : ");
         String author = scanner.nextLine();
 
-        foundWiseSaying.setContent(content);
-        foundWiseSaying.setAuthor(author);
+        wiseSayingService.modify(foundWiseSaying, content, author);
 
         System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
     }
