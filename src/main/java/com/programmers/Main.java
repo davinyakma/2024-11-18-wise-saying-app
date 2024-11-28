90607ee5d7eec2b45ac1a5942648dd58c3a1814c
davinyakma <poohcdv3652@naver.com>
2024-11-28 11:30:01 +0900

    Restart with Stream_addWiseSaying for removing duplication(6 step)

--- src/main/java/com/programmers/Main.java
+++ src/main/java/com/programmers/Main.java
@@ -25,16 +25,10 @@ class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        //-------------------------
-        //'등록' 명령어 입력했을때만 실행되는 코드를 while문 밖에 중복해서 실행하면 앱을 실행시켰을때 매번 1번 명언은 자동으로 저장되어 있음.
-        //문제는 actionAdd()로직과 중복되는 코드여서 중복코드가 늘어나면 유지보수가 어려워진다.(배열을 list로 바꿀때 중복코드 다 삭제해야 됨.)
-        int id = ++lastId;
+        //샘플 데이터 2개 생성. 항상 명언1,2번임.
+        addWiseSaying("나의 죽음을 적들에게 알리지 말라", "이순신 장군");
+        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
 
-        WiseSaying wiseSaying = new WiseSaying(id, "나의 죽음을 적들에게 알리지 말라", "이순신 장군");
-
-        wiseSayings[wiseSayingsSize] = wiseSaying;
-        wiseSayingsSize++;
-        //-------------------------
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -50,19 +44,26 @@ class App {
         scanner.close();
     }
 
-    void actionAdd() {
-        System.out.print("명언 : ");
-        String content = scanner.nextLine(); //scanner, lastId, wiseSayings, wiseSayingsSize는 run함수와 공유해야 함. 따라서 run()과 actionAdd()바깥으로 빼낸다.
-        System.out.print("작가 : ");
-        String author = scanner.nextLine();
-        int id = ++lastId;
+    //데이터 저장 방식이 배열에서 리스트로 바뀌어도 actionAdd에선 사용자에게 정보를 받는 역할만 하고
+    //이 함수에서 명언을 저장하는 방법을 구현하면 이 코드만 유지보수 하면 됨.
+    WiseSaying addWiseSaying(String content, String author) {
+        int id = ++lastId; //번호는 자동으로 매겨지는 것임. 매개변수로 전달받을 필요 없음.
 
         WiseSaying wiseSaying = new WiseSaying(id, content, author);
 
         wiseSayings[wiseSayingsSize] = wiseSaying;
         wiseSayingsSize++;
 
-        System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+        return wiseSaying; //'n'번 명언이 등록되었음을 출력하려면 id가 리턴되어야 하는데 이 함수에서 명언 wiseSaying을 생성하니깐 명언을 리턴하면 id를 조회할 수 있음.
+    }
+    void actionAdd() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine(); //scanner, lastId, wiseSayings, wiseSayingsSize는 run함수와 공유해야 함. 따라서 run()과 actionAdd()바깥으로 빼낸다.
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+        WiseSaying wiseSaying = addWiseSaying(content, author); //명언을 생성한 뒤 리모컨을 저장해서 리모컨이 가리키는 id를 조회 가능함.
+
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.id));
     }
 
     void actionList() {
