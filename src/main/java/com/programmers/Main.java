47495517a5368fa107894c3bc15eba50c55fd561
davinyakma <poohcdv3652@naver.com>
2024-11-28 11:43:29 +0900

    Restart with Stream_private or public(6 step)

--- src/main/java/com/programmers/Main.java
+++ src/main/java/com/programmers/Main.java
@@ -10,19 +10,19 @@ public class Main {
 }
 
 class App {
-    Scanner scanner;
-    int lastId;
-    WiseSaying[] wiseSayings;
-    int wiseSayingsSize;
+    private Scanner scanner;
+    private int lastId;
+    private WiseSaying[] wiseSayings;
+    private int wiseSayingsSize;
 
-    App() {
+    public App() { //생성자는 외부(Main클래스)에서 호출하기 때문에 public
         scanner = new Scanner(System.in);
         lastId = 0;
         wiseSayings =  new WiseSaying[100];
         wiseSayingsSize = 0;
     }
 
-    public void run() {
+    public void run() { //run메소드로 외부(Main클래스)에서 호출하므로 public
         System.out.println("== 명언 앱 ==");
 
         //샘플 데이터 2개 생성. 항상 명언1,2번임.
@@ -44,29 +44,28 @@ class App {
         scanner.close();
     }
 
-    //데이터 저장 방식이 배열에서 리스트로 바뀌어도 actionAdd에선 사용자에게 정보를 받는 역할만 하고
-    //이 함수에서 명언을 저장하는 방법을 구현하면 이 코드만 유지보수 하면 됨.
-    WiseSaying addWiseSaying(String content, String author) {
-        int id = ++lastId; //번호는 자동으로 매겨지는 것임. 매개변수로 전달받을 필요 없음.
+    // App내부에서만 호출하므로 private사용
+    private WiseSaying addWiseSaying(String content, String author) {
+        int id = ++lastId;
 
         WiseSaying wiseSaying = new WiseSaying(id, content, author);
 
         wiseSayings[wiseSayingsSize] = wiseSaying;
         wiseSayingsSize++;
 
-        return wiseSaying; //'n'번 명언이 등록되었음을 출력하려면 id가 리턴되어야 하는데 이 함수에서 명언 wiseSaying을 생성하니깐 명언을 리턴하면 id를 조회할 수 있음.
+        return wiseSaying;
     }
-    void actionAdd() {
+    private void actionAdd() {
         System.out.print("명언 : ");
-        String content = scanner.nextLine(); //scanner, lastId, wiseSayings, wiseSayingsSize는 run함수와 공유해야 함. 따라서 run()과 actionAdd()바깥으로 빼낸다.
+        String content = scanner.nextLine();
         System.out.print("작가 : ");
         String author = scanner.nextLine();
-        WiseSaying wiseSaying = addWiseSaying(content, author); //명언을 생성한 뒤 리모컨을 저장해서 리모컨이 가리키는 id를 조회 가능함.
+        WiseSaying wiseSaying = addWiseSaying(content, author);
 
         System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.id));
     }
 
-    void actionList() {
+    private void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
@@ -77,12 +76,12 @@ class App {
     }
 }
 
-class WiseSaying extends Object{
-    int id;
-    String content;
-    String author;
+class WiseSaying { //명언에 필요한 정보들은 외부에서 조회하거나 호출하기 때문에 public사용.
+    public int id;
+    public String content;
+    public String author;
 
-    WiseSaying(int id, String content, String author) {
+    public WiseSaying(int id, String content, String author) {
         this.id = id;
         this.content = content;
         this.author = author;
