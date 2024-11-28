33a1a2897263bbc97c3a53d8ab56871defb528c2
davinyakma <poohcdv3652@naver.com>
2024-11-28 13:42:09 +0900

    Restart with Stream_ArrayList(6 step)

--- src/main/java/com/programmers/App.java
+++ src/main/java/com/programmers/App.java
@@ -1,18 +1,18 @@
 package com.programmers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
     private int lastId;
-    private final WiseSaying[] wiseSayings;
-    private int wiseSayingsSize;
+    private final List<WiseSaying> wiseSayings;
 
     public App() {
         scanner = new Scanner(System.in);
         lastId = 0;
-        wiseSayings = new WiseSaying[100];
-        wiseSayingsSize = 0;
+        wiseSayings = new ArrayList<>(); //배열은 크기가 유한해서 크기 지정하는 것이 번거로운데 리스트는 크기 지정을 하지 않아도 됨.
     }
 
     public void run() {
@@ -40,9 +40,10 @@ public class App {
         int id = ++lastId;
 
         WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
 
-        wiseSayings[wiseSayingsSize] = wiseSaying;
-        wiseSayingsSize++;
+        System.out.println("wiseSayings = " + wiseSayings); //배열은 wiseSayings하면 주소값이 출력되어서 Arrays.toString을 사용했는데
+        //List는 wiseSayings의 모든 명언 내용을 다 출력해준다.
 
         return wiseSaying;
     }
@@ -61,8 +62,7 @@ public class App {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
-        for (WiseSaying wiseSaying : wiseSayings) {
-            if (wiseSaying == null) break;
+        for (WiseSaying wiseSaying : wiseSayings) { //리스트는 자동으로 크기 조절이 가능해서 wiseSaying이 null인 경우가 없다. wiseSayings의 size에 맞게끔만 loop 돈다.
             System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
         }
     }
