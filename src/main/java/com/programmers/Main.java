821c9526e5fbabb848cbe30b75ad507818bf0cda
davinyakma <poohcdv3652@naver.com>
2024-11-28 11:50:39 +0900

    Restart with Stream_structure modification(6 step)

--- src/main/java/com/programmers/Main.java
+++ src/main/java/com/programmers/Main.java
@@ -1,95 +1,9 @@
 package com.programmers;
 
-import java.util.Scanner;
-
+// 같은 패키지 안에 존재하는 여러 형재파일들은 import문 없이 서로의 메소드를 호출 가능하다.
 public class Main {
     public static void main(String[] args) {
         App app = new App();
         app.run();
     }
 }
-
-class App {
-    private Scanner scanner;
-    private int lastId;
-    private WiseSaying[] wiseSayings;
-    private int wiseSayingsSize;
-
-    public App() { //생성자는 외부(Main클래스)에서 호출하기 때문에 public
-        scanner = new Scanner(System.in);
-        lastId = 0;
-        wiseSayings =  new WiseSaying[100];
-        wiseSayingsSize = 0;
-    }
-
-    public void run() { //run메소드로 외부(Main클래스)에서 호출하므로 public
-        System.out.println("== 명언 앱 ==");
-
-        //샘플 데이터 2개 생성. 항상 명언1,2번임.
-        addWiseSaying("나의 죽음을 적들에게 알리지 말라", "이순신 장군");
-        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
-
-        while (true) {
-            System.out.print("명령) ");
-            String cmd = scanner.nextLine();
-
-            if (cmd.equals("종료")) {
-                break;
-            } else if (cmd.equals("등록")) {
-                actionAdd();
-            } else if (cmd.equals("목록")){
-                actionList();
-            }
-        }
-        scanner.close();
-    }
-
-    // App내부에서만 호출하므로 private사용
-    private WiseSaying addWiseSaying(String content, String author) {
-        int id = ++lastId;
-
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-
-        wiseSayings[wiseSayingsSize] = wiseSaying;
-        wiseSayingsSize++;
-
-        return wiseSaying;
-    }
-    private void actionAdd() {
-        System.out.print("명언 : ");
-        String content = scanner.nextLine();
-        System.out.print("작가 : ");
-        String author = scanner.nextLine();
-        WiseSaying wiseSaying = addWiseSaying(content, author);
-
-        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.id));
-    }
-
-    private void actionList() {
-        System.out.println("번호 / 작가 / 명언");
-        System.out.println("----------------------");
-
-        for(WiseSaying wiseSaying : wiseSayings) {
-            if( wiseSaying == null) break;
-            System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
-        }
-    }
-}
-
-class WiseSaying { //명언에 필요한 정보들은 외부에서 조회하거나 호출하기 때문에 public사용.
-    public int id;
-    public String content;
-    public String author;
-
-    public WiseSaying(int id, String content, String author) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
-
-    @Override
-    public String toString() {
-        return "WiseSaying (id=%d, content=\"%s\", author=\"%s\")".formatted(id, content, author);
-    }
-}
-
